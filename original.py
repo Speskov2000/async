0421d359b47fa8c4a096d572fb831ea6484aa18e
inauris <inauris@protonmail.com>
2022-04-01 15:12:11 +0600

    Implement custom event loop

--- original.py
+++ original.py
@@ -4,13 +4,13 @@ import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 5000))
+server_socket.bind(("localhost", 5000))
 server_socket.listen()
 
 while True:
-    print('Before .accept()')
+    print("Before .accept()")
     client_socket, addr = server_socket.accept()
-    print('Connection from', addr)
+    print("Connection from", addr)
 
     while True:
         request = client_socket.recv(4096)
@@ -18,10 +18,10 @@ while True:
         if not request:
             break
         else:
-            print('I resposed!!!')
+            print("I resposed!")
             print(request)
-            response = 'Hello world\n'.encode()
+            response = "> My response\n".encode()
             client_socket.send(response)
 
-    print('Outside inner while loop')
+    print("Outside inner while loop")
     client_socket.close()
